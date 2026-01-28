704497e35ee1939d2e7d3c18c51529564efab38e
Asaut <asauturlanakylbek@gmail.com>
2026-01-28 23:32:54 +0500

    update2

---
+++ practice2/loops/for_else.py
@@ -0,0 +1,12 @@
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+
+#ex2
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
---
+++ practice2/loops/for_range.py
@@ -0,0 +1,10 @@
+for x in range(6):
+  print(x)
+
+#ex2
+for x in range(2, 6):
+  print(x)
+
+#ex3
+for x in range(2, 30, 3):
+  print(x)    
