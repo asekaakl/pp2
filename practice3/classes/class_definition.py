e1828023d4b53618b820c3ecc0c7dd93be554305
Asaut <asauturlanakylbek@gmail.com>
2026-02-09 22:55:47 +0500

    update

---
+++ practice3/classes/class_definition.py
@@ -0,0 +1,17 @@
+#create a class
+class MyClass:
+  x = 5
+#Create Object
+p1 = MyClass()
+print(p1.x)
+#Delete Objects
+del p1  
+
+#Multiple Objects
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
---
+++ practice3/inheritance/super_function.py
@@ -0,0 +1,7 @@
+#Use the super() Function
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 2019)
