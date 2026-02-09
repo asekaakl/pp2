fbaa91d507ea5728306580871005b06fcdf08dfc
Asaut <asauturlanakylbek@gmail.com>
2026-02-09 22:58:36 +0500

    upd

--- practice3/inheritance/multiple_inheritance.py
+++ practice3/inheritance/multiple_inheritance.py
@@ -1,13 +1,3 @@
-#Use the super() Function
-class Student(Person):
-  def __init__(self, fname, lname, year):
-    super().__init__(fname, lname)
-    self.graduationyear = year
-
-x = Student("Mike", "Olsen", 2019)
-
-
-
 class Student(Person):
   def __init__(self, fname, lname, year):
     super().__init__(fname, lname)
