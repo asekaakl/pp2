a8b1275deb30f36b9efba74c7a9e494da30123f9
Asaut <asauturlanakylbek@gmail.com>
2026-02-23 21:21:24 +0500

    update2

---
+++ practice4/dates/dates.py
@@ -0,0 +1,28 @@
+#Python Dates
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+#Date Output
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.year)
+print(x.strftime("%A"))
+
+#Creating Date Objects
+import datetime
+
+x = datetime.datetime(2020, 5, 17)
+
+print(x)
+
+
+#The strftime() Method
+import datetime
+
+x = datetime.datetime(2018, 6, 1)
+
+print(x.strftime("%B"))
---
+++ practice4/math/math.py
@@ -0,0 +1,41 @@
+#Built-in Math Functions
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+#The abs() Function
+x = abs(-7.25)
+
+print(x)
+
+#The pow() Function
+x = pow(4, 3)
+
+print(x)
+
+
+#The Math Module
+
+import math
+
+x = math.sqrt(64)
+
+print(x)
+
+
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+
+import math
+
+x = math.pi
+
+print(x)
